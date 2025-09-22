03da7f1fbdc2511e0957ad08458546f0f08e7b4e
Princess Althea <andicolminas1@gmail.com>
2025-09-22 20:54:11 +0800

    Add tasks app with home page, templates, and static files

--- projB/tasks/views.py
+++ projB/tasks/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def home(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/home.html', {'tasks': tasks})
